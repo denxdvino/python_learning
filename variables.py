1e230314606cfabc1cae1c320d0c6a922cfd7451
Denis <13942denis@gmail.com>
2025-06-30 23:50:51 +0400

    Добавил файл с переменными и вводом

---
+++ variables.py
@@ -0,0 +1,5 @@
+﻿name = input("Как тебя зовут?: ")
+age = int(input("Сколько тебе лет?: "))
+height = float(input("Какой у тебя рост в см?: "))
+
+print(f'Привет, {name}! Тебе {age}, Твой рост - {height}')
